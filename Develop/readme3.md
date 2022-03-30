e73f24ca9f63b55f3dbe5413ebd31c79736f393e
Moses Lee <lee.moses88@gmail.com>
2022-03-30 16:43:05 -0500

    terminal questions completed

---
+++ Develop/readme3.md
@@ -0,0 +1,34 @@
+
+<h1 align="center">undefined </h1>
+
+![badge](https://img.shields.io/badge/license-Apache-brightgreen)<br />
+## Description
+sdf
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+sdf
+## Usage
+sdf
+## License
+![badge](https://img.shields.io/badge/license-Apache-brightgreen)
+<br />
+This application is covered by the Apache license. 
+## Contributing
+sdf
+## Tests
+sdf
+## Questions
+sdf<br />
+<br />
+octocat: Find me on GitHub: [moseslee](https://github.com/moseslee)<br />
+<br />
+Email me with any questions: lee<br /><br />
+
+  
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,37 @@
+function generateReadme(answers) {
+  return `
+<h1 align="center">${answers.itle} </h1>
+
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+## Description
+${answers.description}
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+${answers.installation}
+## Usage
+${answers.usage}
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+<br />
+This application is covered by the ${answers.license} license. 
+## Contributing
+${answers.contributing}
+## Tests
+${answers.tests}
+## Questions
+${answers.questions}<br />
+<br />
+Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+<br />
+Email me with any questions: ${answers.email}<br /><br />
+
+  `;
+}
+module.exports = generateReadme;
