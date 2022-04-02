21f1f5a605938ec2e1d888e4874ba1db4483506b
molee08 <97422765+molee08@users.noreply.github.com>
2022-04-02 14:45:07 -0500

    fixing layout of readme template

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,28 +1,10 @@
-// // TODO: Create a function that returns a license badge based on which license is passed in
-// // If there is no license, return an empty string
-// function renderLicenseBadge(license) {}
-
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {}
-
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
-
-// // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   return `# ${data.title}
-
-// `;
-// }
 function generateReadme(answers) {
   return `
-<h1 align="center">${answers.projectTitle} 👋</h1>
+<h1 align="center">${answers.itle} </h1>
 
 ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
 ## Description
-🔍 ${answers.description}
+${answers.description}
 ## Table of Contents
 - [Description](#description)
 - [Installation](#installation)
@@ -32,24 +14,24 @@ function generateReadme(answers) {
 - [Tests](#tests)
 - [Questions](#questions)
 ## Installation
-💾 ${answers.installation}
+${answers.installation}
 ## Usage
-💻 ${answers.usage}
+${answers.usage}
 ## License
 ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
 <br />
 This application is covered by the ${answers.license} license. 
 ## Contributing
-👪 ${answers.contributing}
+${answers.contributing}
 ## Tests
-✏️ ${answers.tests}
+${answers.tests}
 ## Questions
-✋ ${answers.questions}<br />
+${answers.questions}<br />
 <br />
-:octocat: Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
 <br />
-✉️ Email me with any questions: ${answers.email}<br /><br />
-_This README was generated with ❤️ by [README-generator](https://github.com/jpd61/README-generator) 🔥🔥🔥_
+Email me with any questions: ${answers.email}<br /><br />
+
   `;
 }
 module.exports = generateReadme;
